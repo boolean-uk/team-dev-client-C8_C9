2c65c2f5cbdb4e1e439df273f24f9b88cea9cd3b
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-02-14 09:55:34 +0000

    Trying to get the data but with lots of lovely errors :)

--- src/components/CohortsList/index.js
+++ src/components/CohortsList/index.js
@@ -1,9 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { get } from "../../service/apiClient";
 
 function CohortsList() {
+  // TEST DATA
+  const initialValues = [
+    { id: 1, cohortName: "TEST Cohort 1" },
+    { id: 2, cohortName: "TEST Cohort 2" },
+    // { id: 3, cohortName: "Cohort 3" },
+  ];
+
   // STATES
+  const [cohortsList, setCohortsList] = useState(initialValues);
 
   // GLOBAL VARIABLES
   const navigate = useNavigate();
@@ -13,13 +21,34 @@ function CohortsList() {
 
     const cohortData = async () => {
       const res = await get(`cohorts`);
-      console.log(res.data.cohorts);
+      console.log("SERVER RESPONSE: ", res.data.cohorts);
+      const cohorts = res.data.cohorts;
+
+      setCohortsList(cohorts);
+
+      //   console.log("After setCohortsList", cohortsList);
     };
     cohortData();
   }, [navigate]);
+
+  /* {props.store.map((storeItem, index) => (
+    <StoreItem
+      storeItem={storeItem}
+      key={index}
+      addToCart={props.addToCart}
+    />
+  ))} */
+
   return (
     <>
       <h2>Cohorts </h2>
+      <ul>
+        {cohortsList.map((cohortItem, index) => (
+          <li key={index}>
+            <p>{cohortItem.cohortName}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
