56630ba1c2e5cd3229dee2b74ad9973dce395e04
MaybeFreak <michaelpope97@gmail.com>
2023-02-14 12:16:30 +0100

    removed some comments which are nolonger needed

--- src/components/studentCard/index.js
+++ src/components/studentCard/index.js
@@ -2,12 +2,9 @@ import ProfileCircle from "../profileCircle";
 import './style.css';
 
 const StudentCard = ({ person }) => {
-  // person object removed from function so app doesn't crash. Make sure ypu bring person down as a prop into this component
-  // then follow instructions line 13
-  //{ person } - this is the destructured person object.
+  
   const name = `${person.firstName} ${person.lastName}`
   const userInitials = name.match(/\b(\w)/g)
-  //when person is set up uncomment line 9, 10 and 14 and add the destructured person object into line 7.
 
   return (
     <li className="personCard">
